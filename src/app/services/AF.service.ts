88f3bab17a70d1de350f9d239863698107fde3b3
Vlad Raykovsky <vraykovsky2016@gmail.com>
2018-12-05 19:44:26 +0200

    angular twitter dev dependencies

--- src/app/services/AF.service.ts
+++ src/app/services/AF.service.ts
@@ -3,6 +3,7 @@ import {Injectable} from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 
 import * as firebase from 'firebase';
+import TwitterAuthProvider = firebase.auth.TwitterAuthProvider;
 
 
 @Injectable({providedIn: 'root'})
@@ -12,7 +13,11 @@ export class AF {
 
   socialLogin(loginProvider) {
     if (loginProvider === 'twitter') {
-          return this.af.auth.signInWithPopup(new firebase.auth.TwitterAuthProvider());
+          return this.af.auth.signInWithPopup( new TwitterAuthProvider()).then( data => {
+            return data;
+          }).catch( e => {
+            console.log(e);
+          });
     }
   }
 
